2449899ddfd8ee31c4862ebe754ddbdf9d7262eb
AleksiDu <aleksi.duluzauri@gmail.com>
2022-11-02 21:03:51 +0400

    add modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,12 @@ module.exports = {
   ** Nuxt.js modules
   */
   modules: [
+    '@nuxtjs/axios'
   ],
+
+  axios: {
+    baseURL: process.env.BASE_URL || 'https://nuxt2-app-default-rtdb.firebaseio.com'
+  },
 
   /*
   ** Build configuration
