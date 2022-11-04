170431e7f058a3710769b97addf303df01f03c55
AleksiDu <aleksi.duluzauri@gmail.com>
2022-11-04 14:54:19 +0400

    add user login

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,75 +1,75 @@
-const pkg = require('./package')
+const pkg = require("./package");
 
 module.exports = {
-  mode: 'universal',
+  mode: "universal",
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css?family=Open+Sans" }
-    ]
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Open+Sans",
+      },
+    ],
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#3B8070', height:'4px', duration: 5000 },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#3B8070", height: "4px", duration: 5000 },
 
   /*
-  ** Global CSS
-  */
-  css: [
-    '~assets/styles/main.css'
-  ],
+   ** Global CSS
+   */
+  css: ["~assets/styles/main.css"],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '~plugins/core-components.js',
-    '~plugins/date-filter.js'
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: ["~plugins/core-components.js", "~plugins/date-filter.js"],
 
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    '@nuxtjs/axios'
-  ],
+   ** Nuxt.js modules
+   */
+  modules: ["@nuxtjs/axios"],
 
   axios: {
-    baseURL: process.env.BASE_URL || 'https://nuxt2-app-default-rtdb.firebaseio.com'
+    baseURL:
+      process.env.BASE_URL || "https://nuxt2-app-default-rtdb.firebaseio.com",
   },
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
+     ** You can extend webpack config here
+     */
     transpile: [
-      '@sindresorhus/slugify',
-      '@sindresorhus/transliterate',
-      'hast-util-select',
+      "@sindresorhus/slugify",
+      "@sindresorhus/transliterate",
+      "hast-util-select",
     ],
-
   },
   env: {
-    baseUrl: process.env.BASE_URL || 'https://nuxt2-app-default-rtdb.firebaseio.com'
+    baseUrl:
+      process.env.BASE_URL || "https://nuxt2-app-default-rtdb.firebaseio.com",
+    fbAPIkey: "AIzaSyCfYW86nCHMowJU_sc--ZswbaRiNb28XZM",
   },
   transition: {
-    name:'fade',
-    mode: 'out-in'
-  }
-
-}
+    name: "fade",
+    mode: "out-in",
+  },
+  router: {
+    middleware: "log",
+  },
+};
